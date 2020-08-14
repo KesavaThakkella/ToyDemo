f7ba3454d69252af5ec602db18d244acfb21a382
kesavasubash <kesavasubash@gmail.com>
2020-08-14 09:10:12 +0530

    pushing to child branch

---
+++ src/Selenium2.java
@@ -0,0 +1,20 @@
+
+public class Selenium2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+		
+
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+
+
+	}
+
+}
