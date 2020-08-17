4b79ef42c6d4eef349861a756df688453b5ff5d4
kesavasubash <kesavasubash@gmail.com>
2020-08-17 09:26:13 +0530

    vinitha

---
+++ src/Hello2.java
@@ -0,0 +1,13 @@
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+
+	}
+
+}
